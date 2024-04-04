1f8bddb8dfd33a6ec28477653b403ef581a5b546
maha-2000 <mahaalahmad189@gmail.com>
2024-04-04 15:31:45 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Fazaa());
+}
+
+class Fazaa extends StatelessWidget {
+  const Fazaa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+    )
+    );
+  }
+}
